b928881b289388d27d13be04aa29ba999526f51e
Webbeeshive <webbeeshive.team@gmail.com>
2017-11-28 08:02:21 +0530

    Basic FBase integration

--- fpg/src/app/comps/createdesign/createdesign.component.ts
+++ fpg/src/app/comps/createdesign/createdesign.component.ts
@@ -19,17 +19,33 @@ export class CreatedesignComponent implements OnInit {
       heading:'Some heading',
       description:'Some description',
       notes:'',
-      colors:['#990022','#ffffff'],
+      colors:['#990022','#005500'],
       type:'Poster',
       tags:[],
       date:new Date(),
       grade:1,
       file:''
     };
+
+    
+  }
+
+  public populateListOfColors(){
+      
+      const colorBoxContainer = document.getElementsByClassName('color-box-container')[0];
+      
+      colorBoxContainer.innerHTML = "";
+      for(var i=0;i<this.designItem.colors.length;i++){
+        var colorBox = document.createElement('span');
+        colorBox.className = "color-box";
+        colorBox.style.backgroundColor = this.designItem.colors[i];
+        colorBoxContainer.appendChild(colorBox);
+      }
+      
   }
 
   ngOnInit() {
-
+    this.populateListOfColors();
   }
 
 }
