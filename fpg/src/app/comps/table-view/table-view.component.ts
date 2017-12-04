67477f62d92d19634ac303d8f7f76778b31778b3
esutima <sutirtha.marjit@ericsson.com>
2017-12-04 12:35:30 +0530

    delete option added to table view

--- fpg/src/app/comps/table-view/table-view.component.ts
+++ fpg/src/app/comps/table-view/table-view.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { DesignItem } from '../../shared/Datatypes';
 import { Location } from '@angular/common';
 import {FormControl} from '@angular/forms'
 import { Router  } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import {FireBasePropertiesService} from '../../services/fire-base-properties.service';
 
 @Component({
@@ -10,25 +11,50 @@ import {FireBasePropertiesService} from '../../services/fire-base-properties.ser
   templateUrl: './table-view.component.html',
   styleUrls: ['./table-view.component.css']
 })
-export class TableViewComponent implements OnInit {
+export class TableViewComponent implements OnInit,OnChanges {
 
   public firebase:FireBase;
   public ListOfDesign:Array<any>=[];
   private FireBaseDataREF:any = null;
   private router;
+  public cacheClearingRandomNumber='T';
  
-  constructor(private fbps:FireBasePropertiesService,_router:Router,_location:Location) {
+  constructor(private fbps:FireBasePropertiesService,_router:Router,_location:Location,private http:HttpClient) {
     var base = this;
     base.router = _router;
     base.firebase = fbps.getInstanceOfFireBase();
     base.FireBaseDataREF = this.firebase.database().ref(this.fbps.getRefString(1)+'/data');
-    
+    base.cacheClearingRandomNumber = this.fbps.getCacheClearingRandomNumber();
     base.FireBaseDataREF.once('value').then(function(snapShot){
       base.ListOfDesign = snapShot.val();
              
     });
    }
 
+   ngOnChanges(){
+     this.cacheClearingRandomNumber = this.fbps.getCacheClearingRandomNumber();
+   }
+
+   public removeDesign(id:string){
+      if(confirm('Are you sure you want to delete this design')){
+        var formdata = new FormData();
+        var filename = (()=>{
+                      var arr = this.ListOfDesign[id].file.split('/');
+                      return arr[arr.length-1];
+                    })();
+        formdata.append('auth-token',this.fbps.getPHPAuthToken());
+        formdata.append('action','remove');
+        formdata.append('filename',filename);
+
+        this.http.post(this.fbps.getDesignRootFolder()+'feed.php',formdata).subscribe((data)=>{
+          this.FireBaseDataREF.child(id).remove();  
+          
+          window.location.hash = '#/table-view';
+          window.location.reload(); 
+        })
+        //this.FireBaseDataREF.child(id).remove();      
+      }      
+  }
    
 
    openDetailPage(id){
