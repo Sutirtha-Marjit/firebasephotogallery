3dc0a5e4487500e63d9fd796a0916109e452199c
esutima <sutirtha.marjit@ericsson.com>
2017-12-21 16:35:26 +0530

    Log show started with HC

--- fpg/src/app/comps/activity-log/activity-log.component.ts
+++ fpg/src/app/comps/activity-log/activity-log.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FireBasePropertiesService} from '../../services/fire-base-properties.service';
 
 @Component({
   selector: 'app-activity-log',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity-log.component.css']
 })
 export class ActivityLogComponent implements OnInit {
+  
+  public firebase:FireBase;
+  private FireBaseDataREF:any = null;
+  private hc:Highcharts=null;
+  
+  constructor(private fbps:FireBasePropertiesService) {
+    let base = this;
+    base.firebase = fbps.getInstanceOfFireBase();
+    base.FireBaseDataREF = this.firebase.database().ref(this.fbps.getRefString(1)+'/log');
 
-  constructor() { }
+    base.FireBaseDataREF.once('value').then(function(snapShot){
+      console.log(snapShot.val());
+
+    });
+
+   }
 
   ngOnInit() {
+    //this.hc = window['Highcharts'];
   }
 
 }
